8db1bc2814d3b07173e2ed0a7ac2df5f6fdddf8f
vijeshnk <pace@PW062.local>
2024-01-23 23:09:51 +0530

    created the login page

---
+++ app/login/page.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+import React from 'react'
+import loginImage from '../assests/login.jpg'
+
+const Login = () => {
+    return (
+        <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
+            <div className='hidden sm:block'><Image className='w-full h-full object-cover' alt='gym' src={loginImage} /></div>
+            <div className='bg-gray-800 flex flex-col justify-center'>
+                <form className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
+                    <h2 className='text-4xl text-white font-bold text-center'>SIGN IN</h2>
+                    <div className='flex flex-col text-gray-400 py-2'>
+                        <label>User Name</label>
+                        <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type='text' />
+                    </div>
+                    <div className='flex flex-col text-gray-400 py-2'>
+                        <label>Password</label>
+                        <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type='password' />
+                    </div>
+                    <div className='flex justify-between text-gray-400 py-2'>
+                        <p className='flex items-center'><input className='mr-2' type='checkbox' />Remember Me</p>
+                        <p>Forgot Password</p>
+                    </div>
+
+                    <button className='w-full my-5 py-2 bg-yellow-500 shadow-lg  hover:shadow-yellow-500/40 text-white font-semibold rounded-lg'>Sign In</button>
+                </form>
+            </div>
+
+        </div>
+    )
+}
+
+export default Login
