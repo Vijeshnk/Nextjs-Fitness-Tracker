7876fb3de1e0f160d00eb0a3113e01f7364ef106
vijeshnk <pace@PW062.local>
2024-01-29 21:46:29 +0530

    added the login api

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,23 +1,49 @@
+'use client';
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 import loginImage from '../assests/login.jpg'
+import { setCookie } from 'cookies-next';
 import SignInButton from '../components/SignInButton/SignInButton'
 import Link from 'next/link'
 
+
+interface LoginForm {
+    email: string;
+    password: string;
+}
+
+
 const Login = () => {
+
+    const { register, handleSubmit } = useForm<LoginForm>();
+    const [error, setError] = useState('');
+    const router = useRouter();
+
+    const onSubmit = async (data: LoginForm) => {
+        try {
+            const response = await axios.post('http://localhost:3000/auth/login', data);
+            setCookie('token', response.data.token);
+            router.push('/dashboard'); 
+        } catch (err) {
+            setError('Failed to log in. Please check your credentials.');
+        }
+    };
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 h-screen w-full'>
             <div className='hidden sm:block'><Image className='w-full h-full object-cover' alt='gym' src={loginImage} priority /></div>
             <div className='bg-gray-800 flex flex-col justify-center'>
-                <form className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg'>
+                <form className='max-w-[400px] w-full mx-auto bg-gray-900 p-8 px-8 rounded-lg' onSubmit={handleSubmit(onSubmit)}>
                     <h2 className='text-4xl text-white font-bold text-center'>Sign In</h2>
                     <div className='flex flex-col text-gray-400 py-2'>
-                        <label>User Name</label>
-                        <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type='text' autoComplete='username' />
+                        <label>Email</label>
+                        <input {...register("email")} className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type='email' autoComplete='email' />
                     </div>
                     <div className='flex flex-col text-gray-400 py-2'>
                         <label>Password</label>
-                        <input className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type='password' autoComplete="current-password" />
+                        <input {...register("password")} className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none' type='password' autoComplete="current-password" />
                     </div>
                     <div className='flex justify-between text-gray-400 py-2'>
                         <p className='flex items-center'><input className='mr-2' type='checkbox' />Remember Me</p>
